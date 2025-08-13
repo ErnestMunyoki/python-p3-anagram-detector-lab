dcf898852447ee85c5825e9505f9f13fb4944528
ErnestMunyoki <ernestmunyoki@gmail.com>
2025-08-13 19:53:03 +0300

    changes made to anagram.py

--- lib/anagram.py
+++ lib/anagram.py
@@ -1,1 +1,13 @@
 # your code goes here!
+
+class Anagram:
+    def __init__(self, word):
+        self.word = word.lower() 
+
+    def match(self, words):
+        matches = []
+        for w in words:
+            if w.lower() != self.word:
+                if sorted(w.lower()) == sorted(self.word):
+                    matches.append(w)
+        return matches
